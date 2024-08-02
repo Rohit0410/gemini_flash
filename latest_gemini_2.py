5a6e013a2afea48f73f60bd37ac72e039d7f7627
Rohit Chauhan <162285388+Rohit0410@users.noreply.github.com>
2024-08-02 18:44:35 +0530

    Update latest_gemini_2.py

--- latest_gemini_2.py
+++ latest_gemini_2.py
@@ -56,7 +56,8 @@ def input_pdf_setup(uploaded_file):
 ## Streamlit App
 
 st.set_page_config(page_title="ATS Resume EXpert")
-st.header("Talent-Match 360")
+st.markdown('''# TalentMatch360 🌟''')
+st.markdown('''### Nextgen tool for evaluating Job Descriptions and Resumes''')
 input_text=st.text_area("Job Description: ",key="input")
 # jd_upload=st.file_uploader("Upload your job description...")
 # if jd_upload is not None:
